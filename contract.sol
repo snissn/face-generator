d40194746bd22d269d8d0664812c9d3d3f8b1ed8
mike seiler <snissn@gmail.com>
2021-09-03 15:32:44 -1000

    new contract with presale

--- contract.sol
+++ contract.sol
@@ -5,12 +5,11 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract ShapeFaces is ERC721, Ownable {
+
+contract ShapeFaces is ERC721, Ownable  {
 
     
-    
-        using Counters for Counters.Counter;
-
+    using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
     
@@ -18,7 +17,47 @@ contract ShapeFaces is ERC721, Ownable {
     
     uint256 public tokenPrice = 29000000000000000; // 0.029 ETH
     uint256 public maxTokens = 7777;
+    uint256 public preSaleTokens = 777+200;
+    bool public mainSaleActive = false; // if false means pre-sale only active
+
     string public baseURI = "https://shapefaces.uc.r.appspot.com/";
+    
+    bool public saleIsActive = false;
+    bool public reserved = false;
+
+    event ShapeFacesPriceChanged(uint256 price);
+    event MaxTokenAmountChanged(uint256 value);
+    event MaxPurchaseChanged(uint256 value);
+    event ShapeFacesReserved();
+    event RolledOver(bool status);
+    
+    
+    function totalSupply() public view returns (uint256){
+        return _tokenIdCounter.current() ;
+    }
+    
+    function reserveShapeFaces() public onlyOwner onReserve {
+        uint supply = totalSupply();
+        uint i;
+        for (i; i < 200; i++) {
+            _safeMint(_msgSender(), supply + i);
+            seeds.push(uint(keccak256(abi.encodePacked(block.difficulty, blockhash(block.number),block.timestamp, _tokenIdCounter.current()))));
+            _tokenIdCounter.increment();
+
+        }
+    }
+    
+    modifier onReserve() {
+        require(!reserved, "Tokens reserved");
+        _;
+        reserved = true;
+        emit ShapeFacesReserved();
+    }
+
+
+    function setPreSaleAmount(uint256 _amt) public onlyOwner {
+        preSaleTokens = _amt;
+    }
     
     function setURI(string memory url) public onlyOwner {
         baseURI = url;
@@ -47,6 +86,9 @@ contract ShapeFaces is ERC721, Ownable {
         require(amount < 11);
         require(_tokenIdCounter.current() + amount <= maxTokens, "Purchase would exceed max supply of ShapeFaces");
         require(tokenPrice * amount == msg.value, "Ether value sent is not correct");
+        if(!mainSaleActive){
+            require(_tokenIdCounter.current() + amount <= preSaleTokens, "Purchase would exceed max pre-sale supply of ShapeFaces");
+        }
 
         for(uint index=0;index < amount; index++){
             _safeMint(msg.sender, _tokenIdCounter.current());
