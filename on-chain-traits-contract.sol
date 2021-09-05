e03613f575a1944b86f5ce794442df18ef29fc8c
mike seiler <snissn@gmail.com>
2021-09-04 15:58:50 -1000

    add more traits

--- on-chain-traits-contract.sol
+++ on-chain-traits-contract.sol
@@ -70,6 +70,47 @@ contract ShapeFaces is ERC721, Ownable  {
         2
     ];
     
+    string[] private backgrounds = [
+          "Worm Hole" ,
+  "Money",
+  "Saturn",
+  "Moon Landing",
+  "Cryptos",
+  "Matrix",
+  "Space Lambo",
+  "Railway",
+  "Bitcoin",
+  "American Flag",
+  "Mount Rushmore",
+  "Manhattan Bridge" ,
+  "Bokeh",
+  "Rainbow",
+  "Galaxy",
+  "Pizza",
+  "Ripple",
+  "Graph Paper",
+  "Rhombus",
+  "Circles",
+  "Triangles",
+  "Circles And Triangles",
+  "Normal"
+    ];
+    
+    uint256[] private backgrounds_probabilities = [2, 5, 5, 5, 10, 10, 10, 10, 15, 15, 15, 20, 20, 60, 60, 60, 60, 100, 100, 200, 200, 200, 100];
+    
+    string[] private genders = [
+        "Male" ,
+        "Female"
+        ];
+    uint256[] private genders_probabilities = [35,65];  
+    function getGender(uint256 tokenId) public view returns (  string memory){
+        return get_probabilities(genders,genders_probabilities, "GENDER", tokenId);
+    }
+    
+    
+    function getBackground(uint256 tokenId) public view returns (  string memory){
+        return get_probabilities(backgrounds,backgrounds_probabilities, "BACKGROUND", tokenId);
+    }
     function getAnimation(uint256 tokenId) public view returns (  string memory){
         return get_probabilities(animations, animations_probabilities, "ANIMATION", tokenId);
     }
@@ -79,11 +120,12 @@ contract ShapeFaces is ERC721, Ownable  {
         uint256 randnum = random(string(abi.encodePacked(trait_name, toString(seeds[tokenId])))) % total;
         uint256 probability_index = 0;
         for(uint i=0; i<traits_probabilities.length; i++) {
-            probability_index += traits_probabilities[i] / total;
+            probability_index += traits_probabilities[i];
             if(randnum < probability_index){
                  return traits[i];
              }
         }
+        require(0==1,"oops math broke");
         return traits[traits.length-1]; // should never hit this but makes compiler happier
     }
     
@@ -108,6 +150,14 @@ contract ShapeFaces is ERC721, Ownable  {
     function setURI(string memory url) public onlyOwner {
         baseURI = url;
     }
+
+    function tokenURI(uint256 tokenId) override public view returns (string memory) {
+        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Shape Face #', toString(tokenId), '", "Animations":"',getAnimation(tokenId),'", "description": "blah", "image": "https://imageapi/"}'))));
+        string memory output = string(abi.encodePacked('data:application/json;base64,', json));
+        return output;
+    }
+
+
 
 
    function withdraw() public onlyOwner {
@@ -174,3 +224,63 @@ contract ShapeFaces is ERC721, Ownable  {
 
 
 
+/// [MIT License]
+/// @title Base64
+/// @notice Provides a function for encoding some bytes in base64
+/// @author Brecht Devos <brecht@loopring.org>
+library Base64 {
+    bytes internal constant TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    /// @notice Encodes some bytes to the base64 representation
+    function encode(bytes memory data) internal pure returns (string memory) {
+        uint256 len = data.length;
+        if (len == 0) return "";
+
+        // multiply by 4/3 rounded up
+        uint256 encodedLen = 4 * ((len + 2) / 3);
+
+        // Add some extra buffer at the end
+        bytes memory result = new bytes(encodedLen + 32);
+
+        bytes memory table = TABLE;
+
+        assembly {
+            let tablePtr := add(table, 1)
+            let resultPtr := add(result, 32)
+
+            for {
+                let i := 0
+            } lt(i, len) {
+
+            } {
+                i := add(i, 3)
+                let input := and(mload(add(data, i)), 0xffffff)
+
+                let out := mload(add(tablePtr, and(shr(18, input), 0x3F)))
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(shr(12, input), 0x3F))), 0xFF))
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(shr(6, input), 0x3F))), 0xFF))
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(input, 0x3F))), 0xFF))
+                out := shl(224, out)
+
+                mstore(resultPtr, out)
+
+                resultPtr := add(resultPtr, 4)
+            }
+
+            switch mod(len, 3)
+            case 1 {
+                mstore(sub(resultPtr, 2), shl(240, 0x3d3d))
+            }
+            case 2 {
+                mstore(sub(resultPtr, 1), shl(248, 0x3d))
+            }
+
+            mstore(result, encodedLen)
+        }
+
+        return string(result);
+    }
+}
